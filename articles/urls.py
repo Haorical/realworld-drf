bc245db039bf65dcdd822d72575337316c4dd57b
haorical <haorical@outlook.com>
2022-07-29 15:49:37 +0800

    artical url

--- articles/urls.py
+++ articles/urls.py
@@ -3,14 +3,17 @@ from articles.views import (
     ArticleViewSet, ArticlesFavoriteAPIView, ArticlesFeedAPIView,
     CommentsListCreateAPIView, CommentsDestroyAPIView, TagListAPIView
 )
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter(trailing_slash=False)
+router.register('articles', ArticleViewSet)
 
 urlpatterns = [
     path('articles', ArticleViewSet.as_view),  # list/
     path('articles/feed', ArticlesFeedAPIView.as_view()),  # feed
-    path('articles/<str:slug>',ArticleViewSet.as_view),  # get create update article
-    path('articles/<str:slug>/comments', CommentsListCreateAPIView.as_view()),  # add get comments
-    path('articles/<str:slug>/comments/<int:id>', CommentsDestroyAPIView.as_view()),  # delete comments
-    path('articles/<str:slug>/favorite', ArticlesFavoriteAPIView.as_view()),  # (un)favorite article
-    path('articles/tags', TagListAPIView.as_view())  # list tags
+    path('articles/<str:article_slug>',ArticleViewSet.as_view),  # get create update article
+    path('articles/<str:article_slug>/comments', CommentsListCreateAPIView.as_view()),  # add get comments
+    path('articles/<str:article_slug>/comments/<int:comment_pk>', CommentsDestroyAPIView.as_view()),  # delete comments
+    path('articles/<str:article_slug>/favorite', ArticlesFavoriteAPIView.as_view()),  # (un)favorite article
+    path('tags', TagListAPIView.as_view())  # list tags
 ]
